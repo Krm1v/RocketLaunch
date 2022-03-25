3c70e32c281b56b6d3fa29475d2ec1930edeece1
Krm1v <vladkarmalov@gmail.com>
2022-03-25 12:09:27 +0100

    - Added animation to rocket - Added sound when start button is pressed - Other improvements

--- RocketLaunch/ViewController.swift
+++ RocketLaunch/ViewController.swift
@@ -6,23 +6,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     //MARK: - @IBOutlets
     
     @IBOutlet weak var rocketView: UIView!
     @IBOutlet weak var rocketImageView: UIImageView!
     @IBOutlet weak var statusLabel: UILabel!
     
+    //MARK: - Properties
+    
+    var player: AVAudioPlayer?
+    
     //MARK: - UIView lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        guard let path = Bundle.main.path(forResource: "launch", ofType: "mp3") else { return }
+        let url = URL(fileURLWithPath: path)
+        player = try? AVAudioPlayer(contentsOf: url)
+        player?.prepareToPlay()
     }
+    
+    //MARK: - @IBAction
     
     @IBAction func startButtonPressed() {
         rocketView.isHidden = false
+        player?.play()
+        statusLabel.isHidden = true
+        let frame = rocketImageView.frame
+        rocketImageView.frame.origin.y = 1000
+        UIView.animate(withDuration: 3.5) {
+            self.rocketImageView.frame = frame
+        } completion: { _ in
+            self.statusLabel.isHidden = false
+        }
     }
 }
 
